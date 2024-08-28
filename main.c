cb81a7eedef15665cd20123e9d7f4173c0872cf9
Aaron <aaron@AarondeMacBook-Air.local>
2024-08-28 20:57:34 +0800

    porting spi interface

--- main.c
+++ main.c
@@ -8,106 +8,169 @@
 // via a loopback.
 
 #include "hardware/dma.h"
+#include "hardware/clocks.h"
 #include "hardware/spi.h"
+#include "hardware/pwm.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
+#include <hardware/gpio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "lvgl.h"
+#include "lv_st7789.h"
+#include "lv_example_get_started.h"
 
-#define TEST_SIZE 1024
+#define PLL_SYS_KHZ (150 * 1000)
+#define LCD_SPI_PORT (spi1)
+#define LCD_DC_PIN (8)
+#define LCD_CS_PIN (9)
+#define LCD_CLK_PIN (10)
+#define LCD_MOSI_PIN (11)
+#define LCD_RST_PIN (12)
+#define LCD_BL_PIN (13)
 
-int main() {
-  // Enable UART so we can print status output
-  stdio_init_all();
-#if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) ||             \
-    !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) ||  \
-    !defined(PICO_DEFAULT_SPI_CSN_PIN)
-#warning spi/spi_dma example requires a board with SPI pins
-  puts("Default SPI pins were not defined");
-#else
+static uint dma_tx;
+static dma_channel_config c;
 
-  printf("SPI DMA example\n");
+static lv_display_t *disp;
+static struct repeating_timer lvgl_timer;
+/* Send short command to the LCD. This function shall wait until the transaction
+ * finishes. */
+void lcd_send_cmd(lv_display_t *disp,
+                  const uint8_t *cmd,
+                  size_t cmd_size,
+                  const uint8_t *param,
+                  size_t param_size)
+{
+    spi_init(LCD_SPI_PORT, 60 * 1000 * 1000);
 
-  // Enable SPI at 1 MHz and connect to GPIOs
-  spi_init(spi0, 1000 * 1000);
-  gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
-  gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
-  gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
-  gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
-  // Make the SPI pins available to picotool
-  bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN,
-                             PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
-  // Make the CS pin available to picotool
-  bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
+    gpio_put(LCD_DC_PIN, 0);
+    gpio_put(LCD_CS_PIN, 0);
+    //* write cmd
+    spi_write_blocking(LCD_SPI_PORT, (uint8_t *) cmd, cmd_size);
 
-  // Grab some unused dma channels
-  const uint dma_tx = dma_claim_unused_channel(true);
-  const uint dma_rx = dma_claim_unused_channel(true);
+    //* write data
+    gpio_put(LCD_DC_PIN, 1);
+    spi_write_blocking(LCD_SPI_PORT, (uint8_t *) param, param_size);
+    gpio_put(LCD_CS_PIN, 1);
+}
 
-  // Force loopback for testing (I don't have an SPI device handy)
-  hw_set_bits(&spi_get_hw(spi_default)->cr1, SPI_SSPCR1_LBM_BITS);
+/* Send large array of pixel data to the LCD. If necessary, this function has to
+ * do the byte-swapping. This function can do the transfer in the background. */
+void lcd_send_color(lv_display_t *disp,
+                    const uint8_t *cmd,
+                    size_t cmd_size,
+                    uint8_t *param,
+                    size_t param_size)
+{
+    spi_init(LCD_SPI_PORT, 60 * 1000 * 1000);
 
-  static uint8_t txbuf[TEST_SIZE];
-  static uint8_t rxbuf[TEST_SIZE];
-  for (uint i = 0; i < TEST_SIZE; ++i) {
-    txbuf[i] = rand();
-  }
+    gpio_put(LCD_DC_PIN, 0);
+    gpio_put(LCD_CS_PIN, 0);
 
-  // We set the outbound DMA to transfer from a memory buffer to the SPI
-  // transmit FIFO paced by the SPI TX FIFO DREQ The default is for the read
-  // address to increment every element (in this case 1 byte = DMA_SIZE_8) and
-  // for the write address to remain unchanged.
+    spi_write_blocking(LCD_SPI_PORT, cmd, cmd_size);
+    gpio_put(LCD_DC_PIN, 1);
+    spi_set_format(LCD_SPI_PORT, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
-  printf("Configure TX DMA\n");
-  dma_channel_config c = dma_channel_get_default_config(dma_tx);
-  channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-  channel_config_set_dreq(&c, spi_get_dreq(spi_default, true));
-  dma_channel_configure(
-      dma_tx, &c,
-      &spi_get_hw(spi_default)->dr, // write address
-      txbuf,                        // read address
-      TEST_SIZE, // element count (each element is of size transfer_data_size)
-      false);    // don't start yet
+    spi_write16_blocking(LCD_SPI_PORT, (uint16_t *) param, (param_size / 2));
+    lv_display_flush_ready(disp);
+    // dma_channel_configure(dma_tx, &c, &spi_get_hw(LCD_SPI_PORT)->dr,
+    //                       (void *) param,  // read address
+    //                       (param_size / 2), true);
+}
 
-  printf("Configure RX DMA\n");
-
-  // We set the inbound DMA to transfer from the SPI receive FIFO to a memory
-  // buffer paced by the SPI RX FIFO DREQ We configure the read address to
-  // remain unchanged for each element, but the write address to increment (so
-  // data is written throughout the buffer)
-  c = dma_channel_get_default_config(dma_rx);
-  channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-  channel_config_set_dreq(&c, spi_get_dreq(spi_default, false));
-  channel_config_set_read_increment(&c, false);
-  channel_config_set_write_increment(&c, true);
-  dma_channel_configure(
-      dma_rx, &c,
-      rxbuf,                        // write address
-      &spi_get_hw(spi_default)->dr, // read address
-      TEST_SIZE, // element count (each element is of size transfer_data_size)
-      false);    // don't start yet
-
-  printf("Starting DMAs...\n");
-  // start them exactly simultaneously to avoid races (in extreme cases the FIFO
-  // could overflow)
-  dma_start_channel_mask((1u << dma_tx) | (1u << dma_rx));
-  printf("Wait for RX complete...\n");
-  dma_channel_wait_for_finish_blocking(dma_rx);
-  if (dma_channel_is_busy(dma_tx)) {
-    panic("RX completed before TX");
-  }
-
-  printf("Done. Checking...");
-  for (uint i = 0; i < TEST_SIZE; ++i) {
-    if (rxbuf[i] != txbuf[i]) {
-      panic("Mismatch at %d/%d: expected %02x, got %02x", i, TEST_SIZE,
-            txbuf[i], rxbuf[i]);
+static void dma_handler(void)
+{
+    if (dma_channel_get_irq0_status(dma_tx)) {
+        dma_channel_acknowledge_irq0(dma_tx);
+        gpio_put(LCD_CS_PIN, 1);
+        // lv_display_flush_ready();
     }
-  }
+}
 
-  printf("All good\n");
-  dma_channel_unclaim(dma_tx);
-  dma_channel_unclaim(dma_rx);
-  return 0;
-#endif
+static bool repeating_lvgl_timer_callback(struct repeating_timer *t)
+{
+    lv_tick_inc(5);
+    return true;
+}
+
+int main()
+{
+    //* CLOCK Config
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+    clock_configure(clk_peri, 0,
+                    CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
+                    PLL_SYS_KHZ * 1000, PLL_SYS_KHZ * 1000);
+
+
+    stdio_init_all();
+    //* io enable
+
+    spi_init(LCD_SPI_PORT, 150 * 1000 * 1000);
+    gpio_set_function(LCD_MOSI_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(LCD_CLK_PIN, GPIO_FUNC_SPI);
+
+    gpio_init(LCD_DC_PIN);
+    gpio_set_dir(LCD_DC_PIN, GPIO_OUT);
+    gpio_init(LCD_CS_PIN);
+    gpio_set_dir(LCD_CS_PIN, GPIO_OUT);
+    gpio_init(LCD_RST_PIN);
+
+    gpio_set_dir(LCD_RST_PIN, GPIO_OUT);
+    gpio_put(LCD_RST_PIN, 1);
+    sleep_ms(100);
+    gpio_put(LCD_RST_PIN, 0);
+    sleep_ms(100);
+    gpio_put(LCD_RST_PIN, 1);
+
+    // TODO backlight 100%
+    gpio_init(LCD_BL_PIN);
+    gpio_set_dir(LCD_BL_PIN, GPIO_OUT);
+    gpio_put(LCD_BL_PIN, 1);
+
+
+    // DMA Config
+    dma_tx = dma_claim_unused_channel(true);
+    c = dma_channel_get_default_config(dma_tx);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
+    channel_config_set_dreq(&c, spi_get_dreq(LCD_SPI_PORT, true));
+
+    add_repeating_timer_ms(5, repeating_lvgl_timer_callback, NULL, &lvgl_timer);
+
+    disp = lv_st7789_create(135, 240, LV_LCD_FLAG_NONE, lcd_send_cmd,
+                            lcd_send_color);
+
+    lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
+
+    /* Allocate draw buffers on the heap. In this example we use two partial
+     * buffers of 1/10th size of the screen */
+    lv_color_t *buf1 = NULL;
+    lv_color_t *buf2 = NULL;
+
+    uint32_t buf_size =
+        240 * 135 / 10 *
+        lv_color_format_get_size(lv_display_get_color_format(disp));
+
+    buf1 = lv_malloc(buf_size);
+    if (buf1 == NULL) {
+        LV_LOG_ERROR("display draw buffer malloc failed");
+        return 0;
+    }
+
+    buf2 = lv_malloc(buf_size);
+    if (buf2 == NULL) {
+        LV_LOG_ERROR("display buffer malloc failed");
+        lv_free(buf1);
+        return 0;
+    }
+    lv_display_set_buffers(disp, buf1, buf2, buf_size,
+                           LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+    lv_example_get_started_1();
+
+    while (true) {
+        lv_task_handler();
+        printf("hello\n");
+        sleep_ms(5);
+    }
 }
